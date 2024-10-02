58968872782706214f5ce9bb7101ac9bed11596a
ashok9959 <annagiriashok5@gmail.com>
2024-10-02 15:46:08 +0530

    initial commit

---
+++ src/main/java/com/sg/sf/EcommercesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sg.sf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/sf/EcommercesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.sf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
